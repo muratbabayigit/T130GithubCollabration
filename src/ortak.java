e3eefcfc0bc95bda79afcbfbf7027a700da384c1
Elif Kesen <elifkesen@gmail.com>
2024-02-03 20:46:33 +0100

    elif 03.02.2024 v1

--- src/ortak.java
+++ src/ortak.java
@@ -1,5 +1,8 @@
 public class ortak {
     public static void main(String[] args) {
         System.out.println("Ne yaparsan yap.AŞK ile Yap!");
+
+        System.out.println("Hele bakin, kim gelmis");
     }
+
 }

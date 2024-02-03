1ba0134e3ef35747cb94efc93afc73adf0865db9
babayigit <babayigit@gmail.com>
2024-02-03 22:42:11 +0300

    babayigit 03.02.2024 v1

--- src/ortak.java
+++ src/ortak.java
@@ -1,5 +1,9 @@
 public class ortak {
     public static void main(String[] args) {
         System.out.println("Ne yaparsan yap.AŞK ile Yap!");
+
+        System.out.println("Ben Murat Ben Geldim");
+
+
     }
 }

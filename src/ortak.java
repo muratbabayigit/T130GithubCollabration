7b5a015681a7ff5655d3fc54b50400a1c9def0eb
Recep ALICI <alicirecepp@gmail.com>
2024-02-03 20:42:25 +0100

    recep/03/02/2024/v2

--- src/ortak.java
+++ src/ortak.java
@@ -1,5 +1,9 @@
 public class ortak {
     public static void main(String[] args) {
         System.out.println("Ne yaparsan yap.AŞK ile Yap!");
+
+        System.out.println("Birlikte ogrenerek gelisiyoruz-");
     }
 }
+
+
